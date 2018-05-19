ff78ceac609d53523628d7a750f997a76dc9e8e9
lirui <1176867093@qq.com>
2018-05-19 13:59:36 +0800

    修改了参数

--- defect.py
+++ defect.py
@@ -1,11 +1,13 @@
-from PyQt5.QtWidgets import (QMainWindow, QTextEdit, QAction, 
+from PyQt5.QtWidgets import (QMainWindow, QTextEdit, QAction, QLineEdit,
                             QFileDialog, QApplication, QWidget,
                             QHBoxLayout, QVBoxLayout, QGridLayout,
                             QLabel, QPushButton,
                             QGroupBox, QMessageBox,
-                            QSizePolicy)
+                            QSizePolicy,
+                            QSlider)
 from PyQt5.QtGui import QIcon, QKeySequence, QPixmap
 from PyQt5.QtCore import Qt, QFile, QDataStream, QIODevice, QTextStream
+import os
 
 # from recognition import test_one_image
 
@@ -14,6 +16,7 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.img_path = r'C:\Users\qazwsx\Desktop\GUI\D2_M_570528_00030.jpg'
         self.img_dir = r''
+        self.is_dir = False
         self.txt_path = ''
         self.model_path = ''
         self.initUI()
@@ -36,13 +39,13 @@ class MainWindow(QMainWindow):
         
         main_layout.addWidget(self.imgBox, 0, 0, 3, 3)
         main_layout.addLayout(self.result_layout, 0, 3, 3, 4)
-        main_layout.addLayout(self.loglayout, 3, 0, 1, 7)
+        main_layout.addLayout(self.loglayout, 3, 0, 2, 7)
 
         mainwidget.setLayout(main_layout)
         self.setCentralWidget(mainwidget)
 
 
-        self.setGeometry(300, 100, 700, 400)
+        self.setGeometry(300, 100, 700, 500)
         self.setWindowTitle('缺陷识别')
 
 
@@ -52,6 +55,8 @@ class MainWindow(QMainWindow):
         self.imglabel = QLabel()
         self.imglabel.setAlignment(Qt.AlignHCenter)
         self.imgBox = QGroupBox("defect image")
+        self.imglabel.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
+
         layout = QVBoxLayout()
 
         # pixMap = QPixmap("icon.jpg")
@@ -67,14 +72,21 @@ class MainWindow(QMainWindow):
         self.bigEditor.setReadOnly(True)
         # bigEditor.setPlainText("黑斑黑斑.")
 
+        btn_layout = QHBoxLayout()
+        
+        clear_btn = QPushButton('清除')
+        clear_btn.clicked.connect(self.clear_result)
         shibie_btn = QPushButton('识别')
         shibie_btn.clicked.connect(self.recognition)
-
-        print('显示区就绪')
+        btn_layout.addStretch(1)
+        btn_layout.addWidget(clear_btn)
+        btn_layout.addStretch(1)
+        btn_layout.addWidget(shibie_btn)
+        btn_layout.addStretch(1)
 
         self.result_layout.addWidget(nameLabel)
         self.result_layout.addWidget(self.bigEditor)
-        self.result_layout.addWidget(shibie_btn)
+        self.result_layout.addLayout(btn_layout)
 
     # 日志显示区
     def creatInfoBox(self):
@@ -85,12 +97,12 @@ class MainWindow(QMainWindow):
         loglabel = QLabel('信息')
         self.logEdit = QTextEdit()
         self.logEdit.setReadOnly(True)
+        # self.logEdit.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
         # size = self.logEdit.sizeHint()
         # print(size)
         # size.setHeight(size.height() - 100)
         # self.logEdit.setBaseSize(size)
         
-
         self.loglayout.addWidget(loglabel)
         self.loglayout.addWidget(self.logEdit)
 
@@ -100,7 +112,7 @@ class MainWindow(QMainWindow):
         self.openAct = QAction("&打开图片", self, shortcut=QKeySequence.Open, 
                         statusTip="Open an img", triggered=self.open)
         self.openDirAct = QAction("&打开文件夹", self, 
-                        statusTip="Open a dir of images", triggered=self.open)
+                        statusTip="Open a dir of images", triggered=self.open_dir)
         self.saveAct = QAction( "&保存", self,
                 shortcut=QKeySequence.Save,
                 statusTip="Save the recognition result to disk", triggered=self.save)
@@ -111,6 +123,9 @@ class MainWindow(QMainWindow):
 
         self.exitAct = QAction("E&xit", self, shortcut="Ctrl+Q",
                 statusTip="Exit the application", triggered=self.close)
+        
+        self.settingAct = QAction("设置", self, 
+                statusTip="设置模型参数")
         
         self.aboutAct = QAction("关于", self,
                 statusTip="Show the application's About box",
@@ -131,6 +146,10 @@ class MainWindow(QMainWindow):
         fileMenu.addSeparator()
         fileMenu.addAction(self.exitAct)
 
+        setMenu = menubar.addMenu('设置')
+        setMenu.addAction(self.settingAct)
+
+
         helpMenu = menubar.addMenu('帮助')
         helpMenu.addAction(self.aboutAct)
         helpMenu.addAction(self.helpAct)
@@ -147,7 +166,16 @@ class MainWindow(QMainWindow):
             self.img_path = fname[0]
             pixMap = QPixmap(self.img_path)
             self.imglabel.setPixmap(pixMap)
+        self.is_dir = False
+        self.logEdit.append('已打开图片 %s, 请点击识别<b>按钮</b>开始识别' % fname)
     
+    def open_dir(self):
+        dir_name = QFileDialog.getExistingDirectory(self, '选取文件夹', 'C:/')
+        self.img_dir = dir_name
+        self.is_dir = True
+        self.logEdit.append('已选取文件夹 %s, 请点击识别<b>按钮</b>开始识别' % dir_name)
+
+
 
     def save(self):
         if self.txt_path:
@@ -162,12 +190,6 @@ class MainWindow(QMainWindow):
         return False
     
     # 保存为.txt或者.csv
-    # QFile file( fileName ); // 把文本写入到文件中
-    # if ( file.open( IO_WriteOnly ) ) {
-    #     QTextStream ts( &file );
-    #     ts << textEdit->text();
-    #     textEdit->setModified( FALSE );
-    # }
     def saveFile(self, file_name):
         print(self.bigEditor.toPlainText())
         qfile = QFile(file_name)
@@ -194,16 +216,27 @@ class MainWindow(QMainWindow):
     def about(self):
         QMessageBox.about(self, "About Applocation",
                 "此<b>钢板缺陷识别系统</b>是基于深度学习搭建而成，目前适用于"
-                "折叠、压痕、划伤、结疤、氧化铁皮、黑斑6大钢板缺陷的分类")
+                "折叠、压痕、划伤、结疤、氧化铁皮、黑斑6大钢板缺陷的分类识别")
     
     def abouthelp(self):
         pass
 
     def recognition(self):
-        # pro, defect_name = test_one_image(self.img_path)
-        pro, defect_name = 1, "haha"
-        # self.bigEditor.setPlainText()
-        self.bigEditor.append('%s,%s,%s' % (self.img_path.split('\\')[-1], pro, defect_name))
+        if self.is_dir:
+            img_list = os.listdir(self.img_dir)
+            for i in img_list:
+                p = os.path.join(img_list, i)
+                # pro, defect_name = test_one_image(p)
+                pro, defect_name = 2, "文件夹"
+                self.bigEditor.append('%s,%s,%s' % (p.split('\\')[-1], pro, defect_name))
+        else:
+            # pro, defect_name = test_one_image(self.img_path)
+            pro, defect_name = 1, "haha"
+            # self.bigEditor.setPlainText()
+            self.bigEditor.append('%s,%s,%s' % (self.img_path.split('\\')[-1], pro, defect_name))
+
+    def clear_result(self):
+        self.bigEditor.clear()
 
     def setCurrentFile(self, fileName):
         self.txt_path = fileName
@@ -214,10 +247,49 @@ class MainWindow(QMainWindow):
         #     shownName = self.strippedName(self.curFile)
         # else:
         #     shownName = 'untitled.txt'
-
         # self.setWindowTitle("%s[*] - Application" % shownName)
     
+    def setting_change(self):
+        pass
 
+# 设置窗口
+class SettingWindow(QWidget):
+    def __init__(self, parent=None):
+        super(SettingWindow, self).__init__(parent)
+        self.resize(400, 200)
+        self.setWindowTitle('设置')
+        self.setStyleSheet("background: gray")
+
+        self.initUI()
+
+    def initUI(self):
+        glayout = QGridLayout()
+
+        para_label = QLabel('ModlePara:')
+        dropout_label =QLabel('Dropout:')
+
+        para_text = QLineEdit()
+        dropout_text = QLineEdit('1.0')
+        dropout_text.setReadOnly(True)
+
+        sld = QSlider(Qt.Horizontal)
+
+        btn = QPushButton('选择')
+
+        glayout.setVerticalSpacing(10)
+
+        glayout.addWidget(para_label, 1,1,1,2)
+        glayout.addWidget(para_text, 1,3,1,2)
+        glayout.addWidget(btn, 1,5,1,1)
+        glayout.addWidget(dropout_label,2,1,1,2)
+        glayout.addWidget(dropout_text, 2,3,1,1)
+        glayout.addWidget(sld, 2,4,1,2)
+
+        self.setLayout(glayout)
+
+    def handle_click(self):
+        if not self.isVisible():
+            self.show()
 
 
 if __name__ == '__main__':
@@ -225,6 +297,8 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     mainWin = MainWindow()
+    settingWin = SettingWindow()
+    mainWin.settingAct.triggered.connect(settingWin.handle_click)  # 显示设置窗口
     mainWin.show()
     sys.exit(app.exec_())
 
